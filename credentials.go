f9dcf3b7114e3f4c85bcca0bb5044ba7e5189491
Pedro Serrano <pedro.serrano.next@bbva.com>
2018-10-09 11:21:13 +0200

    update credentials to manage the creation of creds at job location folder

--- credentials.go
+++ credentials.go
@@ -14,9 +14,15 @@ type CredentialsManager struct {
 
 const baseCredentialsURL = "/credentials/store/system/domain/%s/"
 const createCredentialsURL = baseCredentialsURL + "createCredentials"
-const deleteCredentialURL = baseCredentialsURL + "credential/%s/delete"
+const deleteCredentialURL = baseCredentialsURL + "credential/%s/doDelete"
 const configCredentialURL = baseCredentialsURL + "credential/%s/config.xml"
 const credentialsListURL = baseCredentialsURL + "api/json"
+
+const baseCredentialsURLforJob = "/job/%s/credentials/store/folder/domain/%s/"
+const createCredentialsURLforJob = baseCredentialsURLforJob + "createCredentials"
+const deleteCredentialURLforJob = baseCredentialsURLforJob + "credential/%s/doDelete"
+const configCredentialURLforJob = baseCredentialsURLforJob + "credential/%s/config.xml"
+const credentialsListURLforJob = baseCredentialsURLforJob + "api/json"
 
 var listQuery = map[string]string{
 	"tree": "credentials[id]",
@@ -95,11 +101,17 @@ type PrivateKeyFile struct {
 }
 
 //List ids if credentials stored inside provided domain
-func (cm CredentialsManager) List(domain string) ([]string, error) {
+func (cm CredentialsManager) List(domain string, jobPath string) ([]string, error) {
 
 	idsResponse := credentialIDs{}
 	ids := make([]string, 0)
-	err := cm.handleResponse(cm.J.Requester.Get(fmt.Sprintf(credentialsListURL, domain), &idsResponse, listQuery))
+	var pathCredentialsURL string
+	if len(jobPath) > 0 {
+		pathCredentialsURL = fmt.Sprintf(credentialsListURLforJob, jobPath, domain)
+	} else {
+		pathCredentialsURL = fmt.Sprintf(pathCredentialsURL, domain)
+	}
+	err := cm.handleResponse(cm.J.Requester.Get(pathCredentialsURL, &idsResponse, listQuery))
 	if err != nil {
 		return ids, err
 	}
@@ -113,9 +125,15 @@ func (cm CredentialsManager) List(domain string) ([]string, error) {
 
 //GetSingle searches for credential in given domain with given id, if credential is found
 //it will be parsed as xml to creds parameter(creds must be pointer to struct)
-func (cm CredentialsManager) GetSingle(domain string, id string, creds interface{}) error {
+func (cm CredentialsManager) GetSingle(domain string, jobPath string, id string, creds interface{}) error {
 	str := ""
-	err := cm.handleResponse(cm.J.Requester.Get(fmt.Sprintf(configCredentialURL, domain, id), &str, map[string]string{}))
+	var pathCredentialsURL string
+	if len(jobPath) > 0 {
+		pathCredentialsURL = fmt.Sprintf(configCredentialURLforJob, jobPath, domain, id)
+	} else {
+		pathCredentialsURL = fmt.Sprintf(configCredentialURL, domain, id)
+	}
+	err := cm.handleResponse(cm.J.Requester.Get(pathCredentialsURL, &str, map[string]string{}))
 	if err != nil {
 		return err
 	}
@@ -124,20 +142,38 @@ func (cm CredentialsManager) GetSingle(domain string, id string, creds interface
 }
 
 //Add credential to given domain, creds must be struct which is parsable to xml
-func (cm CredentialsManager) Add(domain string, creds interface{}) error {
-
-	return cm.postCredsXML(fmt.Sprintf(createCredentialsURL, domain), creds)
+func (cm CredentialsManager) Add(domain string, jobPath string, creds interface{}) error {
+	var pathCredentialsURL string
+	if len(jobPath) > 0 {
+		pathCredentialsURL = fmt.Sprintf(createCredentialsURLforJob, jobPath, domain)
+	} else {
+		pathCredentialsURL = fmt.Sprintf(createCredentialsURL, domain)
+	}
+	return cm.postCredsXML(pathCredentialsURL, creds)
 }
 
 //Delete credential in given domain with given id
-func (cm CredentialsManager) Delete(domain string, id string) error {
-	return cm.handleResponse(cm.J.Requester.PostXML(fmt.Sprintf(deleteCredentialURL, domain, id), "", cm.J.Raw, map[string]string{}))
+func (cm CredentialsManager) Delete(domain string, jobPath string, id string) error {
+	var pathCredentialsURL string
+	if len(jobPath) > 0 {
+		pathCredentialsURL = fmt.Sprintf(deleteCredentialURLforJob, jobPath, domain, id)
+	} else {
+		pathCredentialsURL = fmt.Sprintf(deleteCredentialURL, domain, id)
+	}
+
+	return cm.handleResponse(cm.J.Requester.PostXML(pathCredentialsURL, "", cm.J.Raw, map[string]string{}))
 }
 
 //Update credential in given domain with given id, creds must be pointer to struct which is parsable to xml
-func (cm CredentialsManager) Update(domain string, id string, creds interface{}) error {
+func (cm CredentialsManager) Update(domain string, jobPath string, id string, creds interface{}) error {
+	var pathCredentialsURL string
+	if len(jobPath) > 0 {
+		pathCredentialsURL = fmt.Sprintf(configCredentialURLforJob, jobPath, domain, id)
+	} else {
+		pathCredentialsURL = fmt.Sprintf(configCredentialURL, domain, id)
+	}
 
-	return cm.postCredsXML(fmt.Sprintf(configCredentialURL, domain, id), creds)
+	return cm.postCredsXML(pathCredentialsURL, creds)
 }
 
 func (cm CredentialsManager) postCredsXML(url string, creds interface{}) error {
